5353eda6abcdb6d603a25f43c82a68a722ae6d4c
lekla <lekla2905@gmail.com>
2022-07-21 00:03:04 +0200

    Changed polybar and bspwm for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,11 @@
 killall -q polybar
 
 # Launch
-echo "---" | tee -a /tmp/polybar.log
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+# echo "---" | tee -a /tmp/polybar.log
+# polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar --reload example &
+done
 
 echo "Bar launched..."
