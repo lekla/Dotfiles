9ca91a44a0e7e23fc285dd78aadcabbb3101b908
lekla <lekla2905@gmail.com>
2022-07-22 11:43:09 +0200

    Fixed Multi-Monitor Polybar settings

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,8 +7,20 @@ killall -q polybar
 # echo "---" | tee -a /tmp/polybar.log
 # polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$m polybar --reload example &
-done
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+#	MONITOR=$m polybar --reload example &
+#done
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch main bar
+polybar top &
+
+# Launch external bar
+my_external_monitor=$(xrandr --query | grep 'HDMI-0')
+if [[ $my_external_monitor = *connected* ]]; then
+	polybar top_external &
+fi
 
 echo "Bar launched..."
